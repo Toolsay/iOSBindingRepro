3df70f29d71ad3fe88a2d618905d3de81094eac8
Edward Brey <edward.brey@toolsay.com>
2018-07-02 21:13:39 -0500

    ARM64 only.

--- Mobile.iOS.Bindings/PGMobileSDK.linkwith.cs
+++ Mobile.iOS.Bindings/PGMobileSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith("PGMobileSDK.a", LinkTarget.ArmV7, Frameworks = "AudioToolbox AVFoundation ExternalAccessory MediaPlayer Security CoreBluetooth", IsCxx = true)]
+[assembly: LinkWith("PGMobileSDK.a", LinkTarget.Arm64, Frameworks = "AudioToolbox AVFoundation ExternalAccessory MediaPlayer Security CoreBluetooth", IsCxx = true)]
